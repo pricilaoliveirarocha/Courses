b921d6004cf1538c77defc0a22a568f1cd6d14ed
pricila <pricilaoliveiras21@outlook.com.br>
2025-06-28 20:10:57 -0300

    Javascript Fundamentals

--- Javascript/Fundamentals/dataTypes.js
+++ Javascript/Fundamentals/dataTypes.js
@@ -1,7 +1,21 @@
-// * Primitives and Objects
+// * Primitives
 
 let number = 23
 let string = 'Jonas'
 let boolean = true
 let undefined 
 
+// Null = empty value
+
+// Symbol (ES2015) = Value that is unique and cannot be changed
+
+// BigInt (ES2020) = Larger integers than the Number type can hold 
+
+// * Javascript has dynamic typing
+
+/*
+CommentsComments
+CommentsCommentsComment
+CommentsComments
+CommentsComments
+*/
